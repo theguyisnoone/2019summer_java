9abd72a97299d1ac165276bc87d5212b11542499
hajaking <953258481@qq.com>
2019-07-22 16:10:27 +0800

    完善用户输入
    valueOf()

--- TestEnum/src/test.java
+++ TestEnum/src/test.java
@@ -2,25 +2,34 @@ import java.util.Scanner;
 
 public class test {
 	 public static void main(String[] args) {
-//		    System.out.println("print a type:");
-//		    Scanner scan=new Scanner(System.in);
-//		    String type=scan.nextLine();
-	        HeroType heroType = HeroType.TANK;
-	        switch (heroType) {
-	        case TANK:
-	            System.out.println("tank");
-	            break;
-	        case WIZARD:
-	            System.out.println("WIZARD");
-	            break;
-	        case ASSIST:
-	            System.out.println("ASSIST");
-	            break;
-	        case PUSH:
-	            System.out.println("PUSH");
-	            break;
+		    System.out.println("print a type:");
+		    Scanner scan=new Scanner(System.in);
+		    String type=scan.nextLine();
+//	        HeroType heroType = HeroType.TANK;
+	        try {
+	        	HeroType heroType=HeroType.valueOf(type);
+	        	switch (heroType) {
+		        case TANK:
+		            System.out.println("tank");
+		            break;
+		        case WIZARD:
+		            System.out.println("WIZARD");
+		            break;
+		        case ASSIST:
+		            System.out.println("ASSIST");
+		            break;
+		        case PUSH:
+		            System.out.println("PUSH");
+		            break;
+		        }
+	        }catch(IllegalArgumentException e) {
+	        	System.out.println("没有该类型");
 	        }
 	        
+	        
+	        
+	        
+	       System.out.println("all:");
 	        //遍历
 	        for(HeroType hero:HeroType.values()) {
 	        	System.out.println(hero);
