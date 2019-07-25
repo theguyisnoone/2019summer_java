66f3efe53fce5abe963c88db025edf702e0b845b
hajaking <953258481@qq.com>
2019-07-25 20:33:57 +0800

    内部类 静态内部类 匿名类 本体类

--- reviewAbstract/src/property/Item.java
+++ reviewAbstract/src/property/Item.java
@@ -1,8 +1,58 @@
 package property;
 
+import property.ClassinClass.notStatic;
+
 public abstract class Item {
+
 	String name;
 	int price;
 	public  abstract boolean disposable();
+	static class test{
+		public void show() {
+		System.out.println("static class!");	
+		}
+	}
 	
+	class notStatic{
+		public void show() {
+			System.out.println("lm not static!");
+		}
+	}
+	
+	public static void main(String[] args) {
+		//static class not need item object
+		Item.test i=new Item.test();
+		i.show();
+		
+		
+		
+		
+		//anonymous class
+		String jaja="jaja";
+		Item xx=new Item() {
+			public boolean disposable() {
+				System.out.println(jaja);
+				return true;
+			}
+		};
+		xx.disposable();
+		//need item object !!class in class  but not static
+		notStatic n=xx.new notStatic();
+		n.show();
+		
+		
+		//local class
+		class Life extends Item{
+			public boolean disposable(){
+				System.out.println("local");
+				return true;
+		
+			}
+			
+		};
+		Life a=new Life();
+		a.disposable();
+		
+		
+	}
 }
