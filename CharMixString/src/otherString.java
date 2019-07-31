b382c6564ec958fd607de348802588e44016f6b9
hajaking <953258481@qq.com>
2019-08-01 07:51:05 +0800

    startsWith endsWith

---
+++ CharMixString/src/otherString.java
@@ -0,0 +1,13 @@
+
+public class otherString {
+	public static void main(String[] args) {
+        String str1 = "the light";
+         
+        String start = "the";
+        String end = "Ight";
+         
+        System.out.println(str1.startsWith(start));//以...开始
+        System.out.println(str1.endsWith(end));//以...结束
+          
+    }
+}
