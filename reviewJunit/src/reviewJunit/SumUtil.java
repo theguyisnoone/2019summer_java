643ad731f3ab2e280a324b654caea1422774dc47
hajaking <953258481@qq.com>
2019-08-05 22:45:27 +0800

    junit

---
+++ reviewJunit/src/reviewJunit/SumUtil.java
@@ -0,0 +1,12 @@
+package reviewJunit;
+
+public class SumUtil {
+	public static int sum1(int i, int j) {
+        return i+j;
+    }
+     
+    public static int sum2(int i, int j, int k) {
+        return i+j+k;
+    }
+     
+}
