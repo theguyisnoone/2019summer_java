90cdef712067e755d71e1ed254b8ae8d5e8d0727
hajaking <953258481@qq.com>
2019-08-05 19:55:10 +0800

    before properties

---
+++ TestLog4j/src/TestLog4j.java
@@ -0,0 +1,27 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class TestLog4j {
+	 static Logger logger=Logger.getLogger(TestLog4j.class);
+	public static void main(String[] args) throws InterruptedException {
+       
+//        System.out.println("trace");
+//        System.out.println("debug");
+//        System.out.println("info");
+//        System.out.println("warn");
+//        System.out.println("error");
+//        System.out.println("fatal");
+		 	BasicConfigurator.configure();
+	        logger.setLevel(Level.DEBUG);
+	        logger.trace("跟踪信息");
+	        logger.debug("调试信息"); 
+	        logger.info("输出信息");
+	        Thread.sleep(1000);
+	        logger.warn("警告信息");
+	        logger.error("错误信息");
+	        logger.fatal("致命信息");
+		
+ 
+    }
+}
